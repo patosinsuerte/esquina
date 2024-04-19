cf630815ed59cfd05ea347ae1036f83c6bcf92e8
Patricio Abarca <bypatoo@gmail.com>
2024-04-19 15:59:16 -0400

    first commit

---
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ActivateLinkColorService } from '../../../shared/services/activateLinkColor.service';
+
+@Component({
+    selector: 'auth-register-page',
+    templateUrl: './register-page.component.html',
+    styleUrl: './register-page.component.scss'
+})
+export class RegisterPageComponent {
+
+    constructor() { }
+
+}
